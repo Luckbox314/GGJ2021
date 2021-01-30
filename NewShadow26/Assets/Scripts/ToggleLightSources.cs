782654eb82eb15d94067e509fa2fe00e1b9ff897
pongmaniatic <37563473+pongmaniatic@users.noreply.github.com>
2021-01-30 16:40:49 +0100

    arregle el script de luces

--- NewShadow26/Assets/Scripts/ToggleLightSources.cs
+++ NewShadow26/Assets/Scripts/ToggleLightSources.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 
 public class ToggleLightSources : MonoBehaviour
 {
+    
+    
     [Header("La fuente de luz y sombras 1")]
-    public GameObject luz1;
-    [Header("La fuente de luz y sombras 2")]
-    public GameObject luz2;
+    public GameObject[] luces;
+
     private bool lightState = false;
     [Header("La fuente de luz y sombras 2")]
     public GameObject noLuz;
+
+    private int luzActual = 0;
 
 
     private void Update()
     {
         if (Input.GetKeyUp("space"))
         {
-            noLuz.SetActive(false);
-            lightState = !lightState;
-            if (lightState) { luz1.SetActive(true); luz2.SetActive(false); }
-            else { luz2.SetActive(true); luz1.SetActive(false); }  
+            if(noLuz != null){noLuz.SetActive(false);}
+            CambiarLuz();
         }
     }
+
+
+    void CambiarLuz()
+    {
+        int numLuces = luces.Length;
+        Debug.Log("numLuces: " + numLuces);
+        Debug.Log("luzActual: " + luzActual);
+        foreach (var luz in luces)
+        {
+            luz.SetActive(false);
+        }
+        luces[luzActual].SetActive(true);
+        luzActual += 1;
+        if (luzActual >= numLuces)
+        {
+            luzActual = 0;
+        }
+        
+
+    }
+
 }
