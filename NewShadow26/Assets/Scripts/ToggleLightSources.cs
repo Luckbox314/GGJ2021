836878496cd55c0122e381126040ebf6662e5610
pongmaniatic <37563473+pongmaniatic@users.noreply.github.com>
2021-01-30 13:58:20 +0100

    added all the shadows and materials

--- NewShadow26/Assets/Scripts/ToggleLightSources.cs
+++ NewShadow26/Assets/Scripts/ToggleLightSources.cs
@@ -1,30 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.LWRP;
 
 public class ToggleLightSources : MonoBehaviour
 {
     [Header("La fuente de luz y sombras 1")]
     public GameObject luz1;
-    public GameObject[] sombras1;
     [Header("La fuente de luz y sombras 2")]
     public GameObject luz2;
-    public GameObject[] sombras2;
-    private bool lightState = true;
+    private bool lightState = false;
+    [Header("La fuente de luz y sombras 2")]
+    public GameObject noLuz;
 
 
     private void Update()
     {
         if (Input.GetKeyUp("space"))
         {
+            noLuz.SetActive(false);
             lightState = !lightState;
             if (lightState) { luz1.SetActive(true); luz2.SetActive(false); }
-            else { luz2.SetActive(true); luz1.SetActive(false); }
-            
-            
+            else { luz2.SetActive(true); luz1.SetActive(false); }  
         }
-
     }
-
 }
