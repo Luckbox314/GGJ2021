a16b785f69c14251ba6ebc91a727a354c01a7c66
pongmaniatic <37563473+pongmaniatic@users.noreply.github.com>
2021-01-30 12:35:39 +0100

    cree una escena y cree un script para cambiar fuentes de luz
    arme el nivel 1 que diseñe yo

---
+++ NewShadow26/Assets/Scripts/ToggleLightSources.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class ToggleLightSources : MonoBehaviour
+{
+    [Header("La fuente de luz y sombras 1")]
+    public GameObject luz1;
+    public GameObject[] sombras1;
+    [Header("La fuente de luz y sombras 2")]
+    public GameObject luz2;
+    public GameObject[] sombras2;
+    private bool lightState = false;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyUp("space"))
+        {
+            lightState = !lightState;
+            if (lightState) { luz1.SetActive(true); luz2.SetActive(false); }
+            else { luz2.SetActive(true); luz1.SetActive(false); }
+            
+            
+        }
+
+    }
+
+}
