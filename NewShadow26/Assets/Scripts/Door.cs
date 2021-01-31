377548a65da7017a0fdac2306f1be2295b5be53c
tocococa <tocococa@gmail.com>
2021-01-31 00:25:32 -0300

    more sfx

--- NewShadow26/Assets/Scripts/Door.cs
+++ NewShadow26/Assets/Scripts/Door.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public int nextLevel;
+    public AudioSource end;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("sad");
         GameManager.gameManager.NextLevel(nextLevel);
+        end.Play();
     }
 }
